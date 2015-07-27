58caef4e92e59949f283fb76e3896007e89371cd
polohb <polohb@gmail.com>
2015-07-27 21:03:20 +0200

    fix more things according to bash best practices

--- simple-fake-raid.sh
+++ simple-fake-raid.sh
@@ -9,34 +9,54 @@
 # 0 5 * * 7 /root/scripts-raid-backup/simple-fake-raid.sh
 #
 
-# log file
-readonly ERROR_FILE_PATH='./error.log'
 
-# User email, diff synch will be send to this email
-readonly MAIL=polohb@gmail.com
 
-# Folder we want to sync
-# Remark : if end by / only the content of the folder is copied
-# else (no / at the end) the folder is copied (1 more lvl in dircetory tree)
-readonly R1_SOURCE_FOLDER='/media/R1_DATA/'
-
-# Destination folder (backuped data)
-readonly R1_DEST_FOLDER='/media/R1_RSYNC'
 
 # Create a mirror folder
-# --delete-after : remove file in dest folder that are not in source folder
-# -z : compress
-# -v : verbose
-# -e sssh : use ssh protocol
-# some example
-#rsync -avz --delete-after /home/source user@ip_du_serveur:/dossier/destination/
-#rsync -av --delete-after $R1_SOURCE_FOLDER $R1_DEST_FOLDER 2> ./error.log
+sync_folder(){
+  local r1SourceFolder = $1;
+  local r1DestFolder = $2;
+  local errorFilePath = $3;
 
-#OUTPUT=`rsync -av --delete-after $R1_SOURCE_FOLDER $R1_DEST_FOLDER`
-#echo $OUTPUT | mail -s "$0 : run" $MAIL;
+  # --delete-after : remove file in dest folder that are not in source folder
+  # -z : compress
+  # -v : verbose
+  # -e sssh : use ssh protocol
+  # some example
+  #rsync -avz --delete-after /home/source user@ip_du_serveur:/dossier/destination/
+  #rsync -av --delete-after $R1_SOURCE_FOLDER $R1_DEST_FOLDER 2> ./error.log
+  # > $errorFilePath 2>&1 : concat error and standart output to error file
+  rsync -av --delete-after ${r1SourceFolder} ${r1DestFolder} > ${errorFilePath} 2>&1
+}
 
-# > $ERROR_FILE_PATH 2>&1 : concat error and standart output to error file
-rsync -av --delete-after ${R1_SOURCE_FOLDER} ${R1_DEST_FOLDER} > ${ERROR_FILE_PATH} 2>&1
+mail_log() {
+  local errorFilePath = $1;
+  local userEmail = $2;
+  # mail the log file to the $MAIL address
+  cat ${errorFilePath} | mail -s "$0 : run results" ${userEmail};
+}
 
-# mail the log file to the $MAIL address
-cat $ERROR_FILE_PATH | mail -s "$0 : run results" $MAIL;
+
+main(){
+  # log file
+  local errorFilePath='./error.log'
+
+  # User email, diff synch will be send to this email
+  local userMail=polohb@gmail.com
+
+  # Folder we want to sync
+  # Remark : if end by / only the content of the folder is copied
+  # else (no / at the end) the folder is copied (1 more lvl in dircetory tree)
+  local r1SourceFolder='/media/R1_DATA/'
+
+  # Destination folder (backuped data)
+  local r1DestFolder='/media/R1_RSYNC'
+
+
+
+  sync_folder ${r1SourceFolder} ${r1DestFolder}  ${errorFilePath}
+  mail_log ${errorFilePath} ${userMail}
+}
+
+
+main
